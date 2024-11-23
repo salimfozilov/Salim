2c40bf03c57839bff28fbbac241a84ac0e80bab5
salimfozilov <nonamesalim707@gmail.com>
2024-11-23 11:55:52 +0500

    Add files via upload

---
+++ qoshimcha dars.2/main.py
@@ -0,0 +1,46 @@
+# 1-masala
+# for i in range(1, 5):
+#     for j in range(1, 5):
+#         print("*", end=" ")
+#     print()
+
+
+
+# 2-masala
+
+# for i in range(1, 5):
+#     for j in range(1, 5):
+#         if j == 1 or i == 4 or j == 4 or i == 1 or i + j == 5:
+#             print('0', end=" ")
+#         else:
+#             print('7', end=" ")
+#     print()
+
+
+
+# 3-masala
+# for i in range(1, 5):
+#     for j in range(1, 5):
+#         if j == 1 or i == 1 or j == 4:
+#            print("#", end=" ")
+#         else:
+#            print("*", end=" ")
+#     print()
+
+
+
+# 4-masala
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         if i + j == 6 or i == j:
+#            print('1', end=" ")
+#         else:
+#             print('0', end=" ")
+#     print()
+
+
+
+# 5-masala
+for i in range(1, 6):
+    for j in range(1, 6):
+        if 
